71c75eeff315f2604726b840222f15edc254f652
Deepak <b.deepakram@gmail.com>
2023-11-10 13:15:16 +0530

    Navbar and react toolkit setup completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,14 @@ import {BrowserRouter } from "react-router-dom"
 import App from './App.jsx'
 import './index.css'
 
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <Provider store={store}>
   <BrowserRouter>
     <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
