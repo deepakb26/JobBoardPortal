e87aacc0ee439ced591ce831c7f5356bcc40bce4
Deepak <b.deepakram@gmail.com>
2023-11-11 00:47:10 +0530

    signup page and components

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import SignUp from "./SignUp"
+import CustomButton from "./CustomButton"
+import TextInput from "./TextInput"
+export { Navbar, Footer, CustomButton, TextInput,SignUp }
--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -1,8 +1,26 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { Office} from "../assets";
 
 const Auth = () => {
+  const {user} = useSelector(state=> state.user)
+  const [open,setOpen] = useState(false)
+  const location = useLocation()
+  let from = location?.state?.from?.path?.pathname || "/";
+
+  if(user.token){ 
+    // logged in
+    return window.location.replace(from);
+     
+  }
   return (
-    <div>Auth</div>
+    
+    <div className='w-full'>
+      <img src ={Office} alt ='Office' className ='object-cotain' />
+    <SignUp open={open} setOpen={setOpen} />
+    
+    </div>
   )
 }
 
