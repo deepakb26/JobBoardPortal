6cbb4bc0afdfb122809dc1cf7d5d36aee071a659
Devesh <deveshk04032004@gmail.com>
2023-11-12 14:28:25 +0530

    Finished working on Header and JobCard

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,7 @@ import Navbar from "./Navbar"
 import Footer from "./Footer"
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
-
-export { Navbar, Footer, CustomButton, TextInput };
+import Header from "./Header";
+import ListBox from "./ListBox";
+import JobCard from "./JobCard";
+export { Navbar, Footer, CustomButton, TextInput, Header, ListBox, JobCard };
