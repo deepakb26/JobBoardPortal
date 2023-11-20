b480b91e75d9833d8a6f4fe292efb5186b0e1dbb
Deepak <53331181+deepakb26@users.noreply.github.com>
2023-11-21 00:04:26 +0530

    Update index.js
    backend deployment

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:8800/api-v1"
+const API_URL = "https://hirehub-jm5a.onrender.com/api-v1"
 
 export const API = axios.create({
     baseURL:API_URL,
